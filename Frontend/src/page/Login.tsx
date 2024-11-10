a43a03a04cd0f5cee342434ec4f6649ef45833bb
holerbah <holerbah@gmail.com>
2024-11-10 12:10:55 +0100

    Implemented login logic on frontend side and storing response token in local storage

--- Frontend/src/page/Login.tsx
+++ Frontend/src/page/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, message } from 'antd';
+import { loginUser } from '../services/userService';
 
 interface LoginFormValues {
 	username: string;
@@ -7,29 +8,43 @@ interface LoginFormValues {
 }
 
 const LoginPage: React.FC = () => {
-	const onFinish = (values: LoginFormValues) => {
-		console.log('Success:', values);
+	const onFinish = async (values: LoginFormValues) => {
+		try {
+			const { username, password } = values;
+			const data = await loginUser(username, password);
+
+			const token = data.token;
+			if (token) {
+				localStorage.setItem('token', token);
+				message.success('Login successful!');
+				setTimeout(() => {
+					window.location.href = '/';
+				}, 1000); 
+			}
+		} catch (error) {
+			message.error('Login failed. Please try again.');
+		}
 	};
 
 	return (
 		<div style={{ maxWidth: '400px', margin: '100px auto' }}>
 			<h2 style={{ textAlign: 'center' }}>Login</h2>
-			<Form name='login' initialValues={{ remember: true }} onFinish={onFinish}>
+			<Form name="login" initialValues={{ remember: true }} onFinish={onFinish}>
 				<Form.Item
-					name='username'
+					name="username"
 					rules={[{ required: true, message: 'Please input your username!' }]}
 				>
-					<Input placeholder='Username' />
+					<Input placeholder="Username" />
 				</Form.Item>
 
 				<Form.Item
-					name='password'
+					name="password"
 					rules={[{ required: true, message: 'Please input your password!' }]}
 				>
-					<Input.Password placeholder='Password' />
+					<Input.Password placeholder="Password" />
 				</Form.Item>
 				<Form.Item>
-					<Button type='primary' htmlType='submit' block>
+					<Button type="primary" htmlType="submit" block>
 						Log in
 					</Button>
 				</Form.Item>
--- Frontend/src/services/userService.ts
+++ Frontend/src/services/userService.ts
@@ -15,3 +15,19 @@ export const postData = async (user: RegistrationUser) => {
 		}
 	}
 };
+
+export const loginUser = async (username: string, password: string) => {
+	try {
+		const url = `${import.meta.env.VITE_USER_BACKEND_URL}/login`;
+		const response = await axios.post(url, { username, password });
+		return response.data; 
+	} catch (error) {
+		if (axios.isAxiosError(error)) {
+			console.error('Error response:', error.response?.data);
+			throw new Error(error.response?.data.message || 'Login failed');
+		} else {
+			console.error('Unexpected error:', error);
+			throw new Error('Unexpected error occurred');
+		}
+	}
+};
