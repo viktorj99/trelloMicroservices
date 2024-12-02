bb3d5a74ed00c68d7a4c55160f5fd43c63e2c70b
Cuturilo01 <markocuturilo001@gmail.com>
2024-12-02 23:48:50 +0100

    Hotfix

--- Backend/users-service/main.go
+++ Backend/users-service/main.go
@@ -7,7 +7,6 @@ import (
 	"users-service/helpers"
 	"users-service/repositories"
 	"users-service/services"
-	"users-service/utils"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -15,10 +14,10 @@ import (
 
 func main() {
 
-	err := utils.LoadCommonPasswordsToConsul("/app/config/common_passwords.txt")
-	if err != nil {
-		log.Fatalf("Failed to load common passwords to Consul: %v", err)
-	}
+	// err := utils.LoadCommonPasswordsToConsul("/app/config/common_passwords.txt")
+	// if err != nil {
+	// 	log.Fatalf("Failed to load common passwords to Consul: %v", err)
+	// }
 
 	logger := log.New(os.Stdout, "INFO: ", log.LstdFlags)
 
